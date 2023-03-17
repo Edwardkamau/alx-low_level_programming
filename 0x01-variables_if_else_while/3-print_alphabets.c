e98c5d0b0f6440c57341719cc53db8e290a1e495
Edwardkamau <edwardkamauirungu@gmail.com>
2023-03-17 09:41:24 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,20 +11,21 @@
 
 int main(void)
 {
-        char c;
+	char c;
 
-        for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
         {
-                putchar(c);
+		putchar(c);
 	}
 
 	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
+
 	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 
 }
