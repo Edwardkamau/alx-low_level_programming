13a03f641bbc90b4d188dbb59a3fe954bf399e4a
Edwardkamau <edwardkamauirungu@gmail.com>
2023-03-17 10:25:55 -0700

    print aphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints a to z without e and q
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+	if (c != 'e' && c != 'q')
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
