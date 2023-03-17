dac243cdfb1f4df3628c42580e901805e1d26438
Edwardkamau <edwardkamauirungu@gmail.com>
2023-03-17 07:53:11 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * x: letter count
+ *
+ * Description: Prints A to Z in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (int c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+
+	return (0);
+
+}
