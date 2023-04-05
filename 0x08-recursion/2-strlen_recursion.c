29ec8e409a4b0d20639820ff446bd111aa7199de
Edwardkamau <edwardkamauirungu@gmail.com>
2023-04-05 14:59:39 -0700

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen_recursion - prints the length of a string
+ *
+ * @s: string
+ *
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	_strlen_recursion(s + 1);
+
+	return (0);
+}
