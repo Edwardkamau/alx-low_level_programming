15c4dec434dc72bda2dadd22de84d8a135799056
Edwardkamau <edwardkamauirungu@gmail.com>
2023-04-17 10:38:34 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - structure of three members
+ *
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
