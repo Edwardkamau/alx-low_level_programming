82eedb07cf512298198def05a896f42069290bea
Edwardkamau <edwardkamauirungu@gmail.com>
2023-05-02 12:30:28 -0700

    delete head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,13 +14,14 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
+	if (!head || !*head)
+		return (0);
+
 	n = (*head)->n;
 
-	while (head)
-	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
-	}
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
 	return (n);
 }
