e708e58397c7b6ac95ebe72575b18db64a252aae
Edwardkamau <edwardkamauirungu@gmail.com>
2023-05-15 03:16:32 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+#include "myfunctions.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: name of file to be created
+ * @text_content: contents to be writtent in created file
+ *
+ * Return: 1 upon success, -1 upon failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, letters_written;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT || O_WRONLY || O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	letters_written = write(fd, text_content, slen(text_content));
+	if (letters_written == -1 || letters_written != slen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (-1);
+}
